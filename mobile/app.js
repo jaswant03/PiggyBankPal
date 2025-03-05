bff7d3d9e00cfc0ce6346d1aef80d86e0117aebf
Jaswant Prabhakaran <PrabhaJ@dhec.sc.gov>
2025-03-05 15:11:26 -0500

    initial commit

---
+++ mobile/app.js
@@ -0,0 +1,28 @@
+//app.js
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { AuthProvider } from './context/AuthContext';
+import LoginScreen from './screens/LoginScreen';
+import SignupScreen from './screens/SignupScreen';
+import HomeScreen from './screens/HomeScreen';
+import BudgetScreen from './screens/BudgetScreen';
+import ReceiptScreen from './screens/ReceiptScreen';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+    return (
+        <AuthProvider>
+            <NavigationContainer>
+                <Stack.Navigator>
+                    <Stack.Screen name="Login" component={LoginScreen} />
+                    <Stack.Screen name="Signup" component={SignupScreen} />
+                    <Stack.Screen name="Home" component={HomeScreen} />
+                    <Stack.Screen name="Budget" component={BudgetScreen} />
+                    <Stack.Screen name="Receipt" component={ReceiptScreen} />
+                </Stack.Navigator>
+            </NavigationContainer>
+        </AuthProvider>
+    );
+}
